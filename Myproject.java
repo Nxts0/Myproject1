1e06828cbb3a022fecef9f52b23a79f93c60fe98
Natsaran <zhinobid@gmail.com>
2023-03-22 11:45:27 +0700

    4line

--- Myproject.java
+++ Myproject.java
@@ -16,6 +16,8 @@ public class Myproject {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Java");
+        System.out.println("Hello programming");
+        System.out.println("wpgg");
     }
     
 }
